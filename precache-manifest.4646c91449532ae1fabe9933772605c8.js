de3e27600596b03ac1680f82e83c20cf5492bcf1
Denison Andre de Sousa <deni.nilso@gmail.com>
2019-11-28 02:12:30 -0300

    Updates

---
+++ precache-manifest.4646c91449532ae1fabe9933772605c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1580b2070b155247370",
+    "url": "/carparking/static/js/runtime~main.7b906c2c.js"
+  },
+  {
+    "revision": "ee4959cf8604c86be9a9",
+    "url": "/carparking/static/js/main.6914991f.chunk.js"
+  },
+  {
+    "revision": "1ee477d8d854130c8b32",
+    "url": "/carparking/static/js/2.826dd32e.chunk.js"
+  },
+  {
+    "revision": "1ee477d8d854130c8b32",
+    "url": "/carparking/static/css/2.eb79f348.chunk.css"
+  },
+  {
+    "revision": "8818cbdd0689557430aaae6adab7309c",
+    "url": "/carparking/index.html"
+  }
+];
